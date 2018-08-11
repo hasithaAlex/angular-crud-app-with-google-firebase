d509f8ec7e63a96be7e96e59234b7b3e743a76d0
hasitha <hasithamadusanka90@gmail.com>
2018-08-11 19:19:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from "@angular/http";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAtt4M9PEioX0xD2Ze9TKHkFEW3DSU3Kpg",
+  authDomain: "chatapp-21d95.firebaseapp.com",
+  databaseURL: "https://chatapp-21d95.firebaseio.com",
+  projectId: "chatapp-21d95",
+  storageBucket: "chatapp-21d95.appspot.com",
+  messagingSenderId: "483452859100"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
